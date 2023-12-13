60fdd9de84fbac7d5990ff8696d8b08dc03bed35
ferdymercury <ferdymercury@users.noreply.github.com>
2023-12-13 16:33:30 +0100

    Fixes https://github.com/root-project/web/issues/942

--- _releases/make-release-page.py
+++ _releases/make-release-page.py
@@ -305,7 +305,7 @@ def generateDownloadInfo(outfile, version, downloadInfo):
     outfile.write("""
 ## Binary distributions
 
-Instead of manually downloading this binary, please explore first whether your [package manager](../install/#install-via-a-package-manager){:target="\_blank"} already provides this version.
+Instead of manually downloading this binary, please explore first whether your [package manager](../../install/#install-via-a-package-manager){:target="\_blank"} already provides this version.
 This way, you will automatically keep up-to-date with the latest stable versions with no manual maintenance on your side.
 
 | Platform       | Files | Size |
